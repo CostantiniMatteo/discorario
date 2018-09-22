d6aed773a4e9a55fd5a962f14b3cf7b37b53893d
Matteo Costantini <matteo.angelo.costantini@gmail.com>
2018-09-23 01:12:36 +0200

    Update new website

--- discorario_bot.py
+++ discorario_bot.py
@@ -1,260 +1,313 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import datetime, io, logging
-from PIL import Image
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
-                          ConversationHandler)
+import six, requests, json, os, sys, imgkit, io, logging
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+from dateutil import rrule
+from copy import deepcopy
+from functools import reduce
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 
+TOKEN = os.environ['TELEGRAM_TOKEN']
+BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
-DRIVER = 'Firefox'
-TOKEN = 'key'
-BASE_URL = "http://orariolezioni.didattica.unimib.it/Orario/Informatica/2017-2018/Settimana-{}_{}/Curricula/{}_{}_{}_Percorsocomune{}_{}.html"
-FIRST_WEEK = datetime.date(2018, 2, 26).isocalendar()[1]
-CDL = {
-        'Informatica': {'Triennale': ('Informatica', 'E3101Q'), 'Magistrale': ('Informatica', 'F1801Q')},
-        'Data Science': ('Datascience', 'F9101Q'),
-        'TTC': ('Teoriaetecnologiadellacomunicazione', 'F9201P')
+url_post = "http://gestioneorari.didattica.unimib.it/PortaleStudentiUnimib//grid_call.php"
+post_form = {
+    "form-type": "corso",
+    "list": 0,
+    "anno": 2018,
+    "scuola": "Informatica",
+    "corso": "F1801Q",
+    "anno2": "GGG|2",
+    "anno2_multi": "GGG|2",
+    "anno2": "GGG|2",
+    "visualizzazione_orario": "cal",
+    "date": "{}",
+    "periodo_didattico": "",
+    "_lang": "en",
+    "all_events": 0,
 }
-USER_PREFERENCES = {}
-COURSE, DEGREE, YEAR, SEMESTER, AL_MZ, CONFIGURATION = range(6)
+
+days = {1: "Lunedì", 2: "Martedì", 3: "Mercoledì", 4: "Giovedì", 5: "Venerdì"}
+
+display_name = {
+    "Advanced machine learning": "Advanced\nmachine learning",
+    "Computer and robot vision": "Computer and\nrobot vision",
+    "Data and computational biology": "Data and\ncomputational biology",
+    "Data and text mining": "Data and text mining",
+    "Evoluzione dei sistemi software e reverse engineering": "Evoluzione dei\nsistemi software e\nreverse engineering",
+    "Information retrieval - t1": "Information retrieval",
+    "Information retrieval - t2": "Information retrieval",
+    "Information retrieval - tugla": "Information retrieval\ntugla",
+    "Intelligenza artificiale": "Intelligenza artificiale",
+    "Laboratorio di interaction design": "Laboratorio di\ninteraction design",
+    "Large scale data management": "Large scale\ndata management",
+    "Tecnologie ed applicazioni dei sistemi distribuiti": "Tecnologie ed applicazioni\ndei sistemi distribuiti",
+    "Ubiquitous and context-aware computing": "Ubiquitous and\ncontext-aware computing",
+    "Visual information processing and management - TU": "Visual information\nprocessing and\nmanagement",
+    "Visual information processing and management - tugea": "Visual information\nprocessing and\nmanagement",
+}
+
+colors = {
+    "Advanced machine learning": "#FFFCB1",
+    "Computer and robot vision": "#FFE6BB",
+    "Data and computational biology": "#B9F4FF",
+    "Data and text mining": "#F3BAF5",
+    "Evoluzione dei sistemi software e reverse engineering": "#F1D0D0",
+    "Information retrieval - t1": "#D5FFA4",
+    "Information retrieval - t2": "#D5FFA4",
+    "Information retrieval - tugla": "#D5FFA4",
+    "Intelligenza artificiale": "#FDCBFE",
+    "Laboratorio di interaction design": "#A0F3A2",
+    "Large scale data management": "#FFE8A4",
+    "Tecnologie ed applicazioni dei sistemi distribuiti": "#FFC6A4",
+    "Ubiquitous and context-aware computing": "#EEC0C0",
+    "Visual information processing and management - TU": "#A7C7D3",
+    "Visual information processing and management - tugea": "#A7C7D3",
+}
+
+with open('css.css') as f:
+    css = f.read()
 
 
-def get_week():
-    return datetime.datetime.now().isocalendar()[1]  - FIRST_WEEK + 1
+def split_hours(start_time, end_time):
+    sdt = datetime.strptime(start_time, "%H:%M")
+    edt = datetime.strptime(end_time, "%H:%M")
+    return list(rrule.rrule(rrule.HOURLY, dtstart=sdt, until=edt))
+
+# 8.30 - 20.30
+hours = split_hours("8:30", "20:30")
+days_list = list(range(1, 6))
+
+day_dict = { k: [] for k in split_hours("8:30", "20:30") }
+
+
+def stub_read_data():
+    with open("response.json") as f:
+        response = json.load(f)
+        lectures = response["celle"]
+        lectures = [
+            {
+                "name": c["nome_insegnamento"],
+                "room": c["codice_aula"],
+                "begin": datetime.strptime(c["ora_inizio"], "%H:%M"),
+                "end": datetime.strptime(c["ora_fine"], "%H:%M"),
+                "day": int(c["giorno"]),
+            }
+            for c in lectures
+        ]
+    return lectures
+
+
+def read_data():
+    response = requests.post(url_post, post_form).json()
+    lectures = response["celle"]
+    lectures = [
+        {
+            "name": c["nome_insegnamento"],
+            "room": c["codice_aula"],
+            "begin": datetime.strptime(c["ora_inizio"], "%H:%M"),
+            "end": datetime.strptime(c["ora_fine"], "%H:%M"),
+            "day": int(c["giorno"]),
+        }
+        for c in lectures
+    ]
+    return lectures
+
+
+def parse_data_by_day(lectures):
+    result = {}
+    for day in days:
+        d = deepcopy(day_dict)
+        filtered = filter(lambda l: l["day"] == str(day), lectures)
+
+        for l in filtered:
+            hours = int((l["end"] - l["begin"]).total_seconds() / 3600)
+            for i in range(hours):
+                d[l["begin"] + timedelta(hours=i)].append(l)
+        result[day] = d
+
+    return result
+
+
+def parse_data_by_hour(lectures):
+    result = {}
+    for hour in hours:
+        result[hour] = []
+        filtered = list(filter(lambda l: l["begin"]  <= hour < l["end"], lectures))
+
+        for day in days_list:
+            day_lectures = list(filter(lambda l: l["day"] == day, filtered))
+            result[hour].append(day_lectures)
+
+    return result
+
+
+def get_html(data_by_hour):
+    result = """
+<table class="tg">
+  <tr>
+    <th class="tg-gcbz">Hour</th>
+    <th class="tg-n562">Monday</th>
+    <th class="tg-n562">Tuestay</th>
+    <th class="tg-n562">Wednesday</th>
+    <th class="tg-n562">Thursday</th>
+    <th class="tg-n562">Friday</th>
+  </tr>
+"""
+
+    for hour in data_by_hour:
+        result += get_row_html(hour, data_by_hour[hour])
+
+
+    result += "</table>"
+    return result
+
+
+def get_row_html(hour, row):
+    result = ""
+
+    overlaps = [len(x) for x in row]
+    max_rowspan = max(1, reduce(lambda x, y: x * y, set(overlaps), 1))
+    first = True
+
+    hour_template = "    <td class='tg-ocds' rowspan='{}'>{}</td>\n"
+    lectures_template = "    <td class='tg-s6z2' rowspan='{}' style='background:{};'>{}<br><br>{}</td>\n"
+
+    for i in range(max(overlaps)):
+        result += "  <tr>\n"
+
+        if first:
+            result += hour_template.format(
+                max_rowspan,
+                hour.strftime("%H:%M")
+            )
+            first = False
+
+        for lectures in row:
+            try:
+                lecture = lectures[i]
+            except IndexError:
+                if len(lectures) == 0:
+                    result += lectures_template.format(max_rowspan, '', '', '')
+                continue
+
+            try:
+                rowspan = max_rowspan / len(lectures)
+            except ZeroDivisionError:
+                rowspan = 1
+
+            result += lectures_template.format(
+                rowspan,
+                colors[lecture['name']],
+                lecture['name'],
+                lecture['room']
+            )
+
+        result += "  </tr>\n"
+
+    return result
+
+
+def get_dataframe(data):
+    df = pd.DataFrame()
+    df["Ora"] = [dt.strftime("%H:%M") for dt in hours]
+    for k in days:
+        day = days[k]
+        df[day] = [
+            display_name[x[0]["name"]] if len(x) > 0 else ""
+            for x in data[k].values()
+        ]
+    return df
+
+
+def get_next_lecture(lectures, query):
+    query = query.lower()
+    now = datetime.now()
+    day = now.weekday() + 1
+    hour = now.hour
+
+    if hour >= 20:
+        day += 1
+        hour = 8
+    if hour < 8:
+        hour = 8
+
+    template = "La prossima lezione di {} è {} alle {} in aula {}"
+    for d in range(day, 6):
+        for h in split_hours(f"{hour}:30", "20:30"):
+            for lecture in lectures[d][h]:
+                if lecture['name'].lower().find(query) >= 0:
+                    return template.format(lecture['name'],
+                                           days[d],
+                                           h.strftime("%H:%M"),
+                                           lecture['room'])
+    else:
+        return "Non ho trovato lezioni per questa settimana"
+
+
+def render_html(filename, html, css):
+    with open('tmp.html', 'w') as text_file:
+        text_file.write(css)
+        text_file.write(html)
+
+    imgkitoptions = {"format": "png"}
+    imgkit.from_file("tmp.html", filename, options=imgkitoptions)
+
 
 
 def start(bot, update):
-    reply_keyboard = [['Data Science', 'TTC'], ["Informatica"]]
-
-    # if update.effective_user.id == 214977134:
-    #     update.message.reply_text("Continuo a non dirtelo a te, Marta...")
-    #     return ConversationHandler.END
-
-    update.message.reply_text("Scegli il corso di laurea",
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-
-    return COURSE
+    help(bot, update)
 
 
-def course(bot, update, user_data):
-    user = update.message.from_user
-    user_data['course'] = update.message.text
-    logger.info("Corso di %s: %s", user.first_name, update.message.text)
-
-    if update.message.text == 'TTC':
-        user_data['degree'] = 'Magistrale'
-        reply_keyboard = [['1', '2']]
-        update.message.reply_text('Anno?',
-            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-        return YEAR
-
-    if update.message.text == 'Data Science':
-        user_data['degree'] = 'Magistrale'
-        user_data['year'] = '1'
-        reply_keyboard = [['1', '2']]
-        update.message.reply_text('Semestre?',
-            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-        return SEMESTER
-
-    if update.message.text == 'Informatica':
-        reply_keyboard = [['Triennale', 'Magistrale']]
-        update.message.reply_text('Triennale o Magistrale?',
-            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-        return DEGREE
-
-    raise Exception('Corso di laurea non valido')
+def help(bot, update):
+    update.message.reply_text("Cerca un corso o scrivi 'orario' per ricevere l'orario settimanale")
 
 
-def degree(bot, update, user_data):
-    user = update.message.from_user
-    user_data['degree'] = update.message.text
-    logger.info("Degree of %s: %s", user.first_name, update.message.text)
+def discorario(bot, update):
+    try:
+        chat_id = update.message.chat_id
+        query = update.message.text.lower()
 
-    reply_keyboard = [['1', '2']]
-    if update.message.text == 'Triennale':
-        reply_keyboard = [['1', '2', '3']]
-    update.message.reply_text('Anno?',
-            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        if update.message.text.find('orario') >= 0:
+            post_form["date"] = f"{datetime.now():%d-%m-%Y}"
+            lectures = parse_data_by_hour(read_data())
+            render_html('tmp.png', get_html(lectures), css)
+            schedule = open('tmp.png', 'rb')
+            bot.send_document(chat_id=chat_id, document=schedule, timeout=10000)
+        else:
+            try:
+                lectures = parse_data_by_day(read_data())
+                response = get_next_lecture(lectures, query)
+            except Exception as e:
+                response = "¯\\_(ツ)_/¯"
+            update.message.reply_text(response)
 
-    return YEAR
-
-
-def year(bot, update, user_data):
-    user = update.message.from_user
-    user_data['year'] = update.message.text
-    logger.info("Year of %s: %s", user.first_name, update.message.text)
-
-    reply_keyboard = [['1', '2']]
-    update.message.reply_text('Semestre?',
-            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-
-    return SEMESTER
-
-
-def semester(bot, update, user_data):
-    user = update.message.from_user
-    user_data['semester'] = update.message.text
-    logger.info("Semester of %s: %s", user.first_name, update.message.text)
-
-    if user_data['degree'] == 'Triennale' and user_data['year'] != 3:
-        reply_keyboard = [['A-L', 'M-Z']]
-        update.message.reply_text('A-L o M-Z?',
-            reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-
-        return AL_MZ
-
-    if 'configuration' in user_data.keys() and user_data['configuration']:
-        save_configuration(user_data)
-        user_data['configuration'] = False
-    else:
-        send_schedule(bot, update, user_data['course'], user_data['degree'], user_data['year'], user_data['semester'])
-    return ConversationHandler.END
-
-
-def al_mz(bot, update, user_data):
-    user = update.message.from_user
-    user_data['al_mz'] = update.message.text
-    logger.info("Group of %s: %s", user.first_name, update.message.text)
-
-    if 'configuration' in user_data.keys() and user_data['configuration']:
-        save_configuration(user_data)
-        user_data['configuration'] = False
-    else:
-        send_schedule(bot, update, user_data['course'], user_data['degree'], user_data['year'], user_data['semester'], user_data['al_mz'])
-
-    return ConversationHandler.END
-
-
-def cancel(bot, update, user_data):
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    update.message.reply_text('Scelta annullata.',
-                              reply_markup=ReplyKeyboardRemove())
-
-    return ConversationHandler.END
+    except Exception as e:
+        error(bot, update, str(e))
 
 
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
 
 
-def send_schedule(bot, update,  course, degree, year, semester, group=''):
-    week = get_week()
-    semester_id = 90 if semester == 1 else 112
-    cdl_id = CDL[course][1] if course != 'Informatica' else CDL[course][degree][1]
-    cdl_name = CDL[course][0] if course != 'Informatica' else CDL[course][degree][0]
-
-    url = BASE_URL.format(week, semester_id, cdl_name, degree.lower(), year, group, cdl_id)
-
-    if DRIVER == 'Firefox':
-        options = Options()
-        options.add_argument('--headless')
-        driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver', firefox_options=options)
-        driver.get(url)
-        driver.set_window_size(1920, 1080)
-        table = driver.find_element_by_class_name('timegrid')
-        image_data = table.screenshot_as_png
-        image = Image.open(io.BytesIO(image_data))
-        result = image
-    else:
-        driver = webdriver.PhantomJS()
-        driver.get(url)
-        driver.set_window_size(1920, 1080)
-        table = driver.find_element_by_class_name('timegrid')
-        x, y = table.location.values()
-        dy, dx = table.size.values()
-        image_data = table.screenshot_as_png
-        image = Image.open(io.BytesIO(image_data))
-        result = image.crop((x, y, x+dx, y+dy)).convert('RGB')
-
-    bio = io.BytesIO()
-    bio.name = "orario_settimana_{}".format(week)
-    result.save(bio, 'PNG')
-    bio.seek(0)
-
-    bot.send_document(chat_id=update.message.chat_id, document=bio)
-
-
-def lazy(bot, update):
-    logger.info("%s is lazy: %s", update.message.from_user.first_name, update.message.text)
-    update.message.reply_text('Invio gli orari della magistrale in informatica (secondo semestre). Per gli altri orari: /start')
-    send_schedule(bot, update, 'Informatica', 'Magistrale', '1', '2')
-
-
-def begin_configuration(bot, update, user_data):
-    logger.info("Configuration (%s): %s", update.message.from_user.first_name, update.message.text)
-    update.message.reply_text('Not implemented')
-    user_data['configuration'] = True
-    return COURSE
-
-def configuration(bot, update, user_data):
-    logger.info("Configuration (%s): %s", update.message.from_user.first_name, update.message.text)
-    update.message.reply_text('Not implemented')
-
-    return ConversationHandler.END
-
-
-def save_configuration(bot, update, user_data):
-    pass
-
-
-def aula(bot, update):
-    logger.info("Aula (%s): %s", update.message.from_user.first_name, update.message.text)
-    if update.effective_user.id not in USER_PREFERENCES.keys():
-        update.message.reply_text('Non conosco i tuoi corsi. Per impostare le preferenze utilizza il comando /configure')
-
-    aula = get_aula(**USER_PREFERENCES[update.effective_user.id])
-
-    return ConversationHandler.END
-
-def get_aula(course, degree, year, semester, al_mz=None, courses=[]):
-    week = get_week()
-
-
 def main():
     updater = Updater(TOKEN)
+
     dp = updater.dispatcher
-
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start),
-                      CommandHandler('configure', begin_configuration, pass_user_data=True),
-                      CommandHandler('aula', aula),
-                      RegexHandler('.+', lazy),
-        ],
-
-        states={
-            COURSE: [RegexHandler('^(Data Science|TTC|Informatica)$',
-                        course, pass_user_data=True)],
-
-            DEGREE: [RegexHandler('^(Triennale|Magistrale)$',
-                        degree, pass_user_data=True)],
-
-            YEAR: [RegexHandler('^(1|2|3)$', year, pass_user_data=True)],
-
-            SEMESTER: [RegexHandler('^(1|2)$', semester, pass_user_data=True)],
-
-            AL_MZ: [RegexHandler('^(A-L|M-Z)$', al_mz, pass_user_data=True)],
-
-            CONFIGURATION: [RegexHandler('.+', configuration, pass_user_data=True)],
-        },
-
-        fallbacks=[CommandHandler('cancel', cancel, pass_user_data=True)]
-    )
-
-    dp.add_handler(conv_handler)
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, discorario))
     dp.add_error_handler(error)
-    updater.start_polling()
 
+    updater.start_polling()
     updater.idle()
 
 
