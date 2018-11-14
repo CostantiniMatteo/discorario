19f6760e6ba3af7499d97e95b2e84d7818c7455a
AndreaBiaggi <biaggi.jack@gmail.com>
2018-11-14 11:43:49 +0100

    set parent directory as base path

--- src/configuration.py
+++ src/configuration.py
@@ -3,7 +3,7 @@ import os
 DEBUG = os.environ.get("DEBUG")
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
-BASE_PATH = os.getcwd()
+BASE_PATH = os.pardir
 RESOURCES = "res"
 DB_PATH = os.path.join(BASE_PATH, "discorario.db")
 CSS_PATH = os.path.join(BASE_PATH, RESOURCES, "css.css")
