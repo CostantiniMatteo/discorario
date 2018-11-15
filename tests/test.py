c3fd27d74e911594d43f4a713ecc71b39c0fea35
AndreaBiaggi <biaggi.jack@gmail.com>
2018-11-15 15:29:56 +0100

    tested user agenda

--- tests/test.py
+++ tests/test.py
@@ -9,6 +9,8 @@ import discorario as do
 from datetime import datetime
 
 def setUpModule():
+    if os.path.exists(conf.DB_PATH):
+        os.remove(conf.DB_PATH)
     dbutil.init()
     do.save_preference("1","F1801Q","Informatica (Magistrali)","Informatica","GGG|2")
 
@@ -23,14 +25,17 @@ class DegreeCoursesTest(unittest.TestCase):
     def test_degree_courses(self):
         self.assertEqual(len(do.get_all_degree_courses()), 71)
 
-class ScheduleTest(unittest.TestCase):
+class AgendaTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        do.save_user_agenda("1",["Information Retrieval","Bioinformatica"])
 
     def test_get_weekly_schedule(self):
-        do.get_weekly_schedule("1", datetime.now())
-        self.assertEqual("ciao","ciao")
+        self.assertEqual(len(do.get_weekly_schedule("1", datetime.now()).courses), 2)
 
-def tearDownModule():
-    os.remove(conf.DB_PATH)
+#def tearDownModule():
+#    os.remove(conf.DB_PATH)
 
 if __name__ == '__main__':
     unittest.main()
